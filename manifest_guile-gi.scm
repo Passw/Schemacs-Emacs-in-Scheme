1f9c40a9bc659286e47c608d8ae05c83bbe8feba
Ramin Honary <ramin.honary@gmail.com>
2025-11-27 09:35:40 +0900

    remove old dependencies from Guix manifest

--- manifest_guile-gi.scm
+++ manifest_guile-gi.scm
@@ -1,20 +1,7 @@
 (specifications->manifest
  '("guile"
    "slib"
-   "guile-chickadee"
    "guile-gi"
-   "guile-goblins"
-   "guile-lib"
    "glib:debug"
    "gtk+"
-
-   ;;"gtk+@2:debug"
-   ;;"gdb"
-   ;;"guile-g-golf"
-   ;;"gtk+"
-
-   ;; *** Note
-   ;; guile-g-golf does not install Gtk by default, and the Guix
-   ;; guile-g-golf package is limited to an old version of libglib
-   ;; which only supports Gtk version 3.
    ))
